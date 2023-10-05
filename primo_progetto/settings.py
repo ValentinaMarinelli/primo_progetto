77ff88fecce9224f65e242bec0d4cb4fcc8629bb
ValentinaMarinelli <valentina.marinelli@alessandrinimainardi.edu.it>
2023-10-05 12:41:39 +0200

    aggiornato parametro allowed host

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fpz(sj09%27^984l^wcjrby9!fv53w*uzw21+db)z=_3^4gnnp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
