00789a139452a299ff0abc668286997c8efbaa15
ValentinaMarinelli <valentina.marinelli@alessandrinimainardi.edu.it>
2023-10-20 08:43:44 +0200

    Aggiornato: Passo 12

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -5,4 +5,6 @@ app_name="seconda_app"
 urlpatterns=[
     path('es_if', es_if, name='es_if'),
     path('if_else_elif', if_else_elif, name='if_else_elif'),
+    path('es_for', es_for, name='es_for'),
+    path('', index_2, name='index_2'),
 ]
--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 
@@ -17,3 +18,14 @@ def if_else_elif(request):
         'var 3' : 100.50,
     }
     return render(request, "if_else_elif.html", context)
+
+def index_2(request):
+    return render(request, "index_2.html")
+
+def es_for(request):
+    context={
+        'list1' : [1, datetime.date(2019,7,16), 'Do not give up!'],
+        'list2' : [1, datetime.date(2019,7,16), 'Do not give up!'],
+        'my_dict' : {'chiave1': 'Valore 1', 'chiave2': 'Valore 2'},
+    }
+    return render(request, "es_for.html", context)
