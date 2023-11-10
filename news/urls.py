14f63a120983589edcf574093c72c96635944849
ValentinaMarinelli <valentina.marinelli@alessandrinimainardi.edu.it>
2023-11-10 10:56:08 +0100

    Passo 18 Parziale

--- news/urls.py
+++ news/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("articoli/<int:pk>", articoloDetailView, name = "articolo_detail"),
     path("lista_articoli/<int:pk>", listaArticoli, name = "lista_articoli"),
     path("lista_articoli/", listaArticoli, name = "lista_articoli"),
+    path("query/", queryBase, name = "query_base"),
 ]
