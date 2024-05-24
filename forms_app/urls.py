5ed449213ee6a194d575297bfcc66cdaa764a195
ValentinaMarinelli <valentina.marinelli@alessandrinimainardi.edu.it>
2024-05-24 10:49:53 +0200

    Passo 25: Concluso

--- forms_app/urls.py
+++ forms_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('contattaci/', contatti, name='contatti'),
     path('', index_contatto, name='index_contatto'),
     path('lista_contatti/', lista_contatti, name='lista_contatti'),
-    path('modifica_contatto/', modifica_contatto, name='modifica_contatto'),
-    path('elimina_contatto/', elimina_contatto, name='elimina_contatto'),
+    path('modifica_contatto/<int:pk>', modifica_contatto, name='modifica_contatto'),
+    path('elimina_contatto/<int:pk>', elimina_contatto, name='elimina_contatto'),
 ]
